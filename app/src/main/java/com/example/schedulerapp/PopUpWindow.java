0417a0de5a7f8b8da9687adf3cf93afe20726109
maxedmonds <maxedmonds@outlook.com>
2024-01-23 13:43:52 -0500

    added drop down options

--- app/src/main/java/com/example/schedulerapp/PopUpWindow.java
+++ app/src/main/java/com/example/schedulerapp/PopUpWindow.java
@@ -8,7 +8,9 @@ import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class PopUpWindow extends Activity {
 
@@ -27,6 +29,20 @@ public class PopUpWindow extends Activity {
                 finish();
             }
         });
+
+        // Display start times in drop down menu
+        Spinner classStartTime = (Spinner) findViewById(R.id.spinnerClassStartTime);
+        String[] startTimes = {"8:00am", "8:30am", "9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm", "4:30pm", "5:00pm"};
+        ArrayAdapter<String> startAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, startTimes);
+        startAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        classStartTime.setAdapter(startAdapter);
+
+        // Display end times in drop down menu
+        Spinner classEndTime = (Spinner) findViewById(R.id.spinnerClassEndTime);
+        String[] endTimes = {"9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm", "4:30pm", "5:00pm", "5:30pm", "6:00pm"};
+        ArrayAdapter<String> endAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, endTimes);
+        endAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        classEndTime.setAdapter(endAdapter);
 
         // Get the metrics for our current display
         DisplayMetrics dm = new DisplayMetrics();
